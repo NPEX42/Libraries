d5209ed509f22e54ecf8d7748ed590bd5d082998
George Venn (Npex42) <npex42@gmail.com>
2020-11-09 10:56:38 +0000

    Branched From main

--- Common/src/np/common/exceptions/ClientException.java
+++ Common/src/np/common/exceptions/ClientException.java
@@ -4,9 +4,11 @@ package np.common.exceptions;
  */
 public class ClientException extends NetworkException {
 	private static final long serialVersionUID = 1L;
+	
 	public ClientException(String message) {
 		super(message);
 	}
+	
 	public ClientException() {
 		this("A Client Exception Has Occured...");
 	}
@@ -14,5 +16,5 @@ public class ClientException extends NetworkException {
 	public static void ThrowConnectionError(String host, int port) { 
 		throw new ClientException("Unable to connect To "+host+":"+port);
 	}
-	
+
 }
